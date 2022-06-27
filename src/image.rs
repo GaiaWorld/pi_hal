997b51fdca5eb5b61164444ff9b778bd80eb044f
wzjsun <530739162@qq.com>
2022-06-27 18:35:49 +0800

    feat：新增接口，用于架加载图片资源

--- src/image.rs
+++ src/image.rs
@@ -1,4 +1,104 @@
-/// 图片
+use std::{ops::{Deref, DerefMut}, io::Error};
+
+use image::DynamicImage;
+use pi_assets::{asset::{Asset, Handle}, mgr::{AssetMgr, LoadResult}};
+use pi_async::rt::AsyncRuntime;
+use pi_async_common::MULTI_RUNTIME; // 后续使用pi_engine中的运行时TODO
+use pi_atom::Atom;
+use pi_share::Share;
+
+pub struct ImageRes {
+	value: DynamicImage,
+	size: usize,
+}
+
+impl Deref for ImageRes {
+    type Target = DynamicImage;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl DerefMut for ImageRes {
+
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
+    }
+}
+
+impl ImageRes {
+	pub fn new(value: DynamicImage) -> Self {
+		let size = match &value {
+			DynamicImage::ImageLuma8(image) => image.width() * image.height() * 4,
+			DynamicImage::ImageLumaA8(image) => image.width() * image.height() * 2,
+			DynamicImage::ImageRgb8(image) => image.width() * image.height() * 3,
+			DynamicImage::ImageRgba8(image) => image.width() * image.height() * 4,
+			DynamicImage::ImageBgr8(image) => image.width() * image.height() * 3,
+			DynamicImage::ImageBgra8(image) => image.width() * image.height() * 4,
+
+			DynamicImage::ImageLuma16(image) => image.width() * image.height() * 2,
+			DynamicImage::ImageLumaA16(image) => image.width() * image.height() * 4,
+
+			DynamicImage::ImageRgb16(image) => image.width() * image.height() * 2 * 3,
+			DynamicImage::ImageRgba16(image) => image.width() * image.height() * 2 * 4,
+		};
+		Self {
+			value,
+			size: size as usize,
+		}
+	}
+}
+
+impl Asset for ImageRes {
+    type Key = Atom;
+
+    fn size(&self) -> usize {
+        self.size
+    }
+}
+
+/// 从本地路径加载图片
+pub async fn load_from_path(
+	mgr: &Share<AssetMgr<ImageRes>>,
+	k: &Atom,
+) -> Result<Handle<ImageRes>, LoadError> {
+	match AssetMgr::load(mgr, &k) {
+		LoadResult::Ok(r) => Ok(r),
+		LoadResult::Wait(f) => match f.await {
+			Ok(r) => Ok(r),
+			Err(e) => Err(LoadError::IoError(e))
+		},
+		LoadResult::Receiver(recv) => {
+			let k1 = k.clone();
+			let wait = MULTI_RUNTIME.wait();
+			wait.spawn(MULTI_RUNTIME.clone(), None, async move {
+				let image = match image::open(k1.as_str()) {
+					Ok(r) => r,
+					Err(e) => {
+						log::error!("load image fail, {:?}", e);
+						return Ok(());
+					}
+				};
+				
+				if let Err(e) = recv.receive(k1, Ok(ImageRes::new(image))).await {
+					log::error!("load image fail, {:?}", e);
+				}
+				Ok(())
+			}).unwrap();
+			wait.wait_result().await.unwrap();
+			match AssetMgr::get(mgr, k) {
+				Some(r) => Ok(r),
+				None => Err(LoadError::Other("load fail".to_string()))
+			}
+		}
+	}
+}
+
+pub enum LoadError {
+	IoError(Error),
+	Other(String),
+}
 
 pub fn from_path(path: &str) -> Result<(Vec<u8>, u32, u32), image::ImageError> {
     let dynamic_image = image::open(path)?;
