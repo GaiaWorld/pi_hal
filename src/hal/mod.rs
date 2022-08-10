f05da5a1c719e35cc1bbd6e0ae5819ef669bb0e1
wzjsun <530739162@qq.com>
2022-08-10 18:19:01 +0800

    fix: 1、升级pi_async版本， 2、修复target cgf判断

--- src/hal/mod.rs
+++ src/hal/mod.rs
@@ -1,13 +1,13 @@
 /// 非wasm32版本，使用本地实现
-#[cfg(all(not(target_feature="wasm32"), not(feature="empty")))]
+#[cfg(all(not(target_arch="wasm32"), not(feature="empty")))]
 mod native;
-#[cfg(all(not(target_feature="wasm32"), not(feature="empty")))]
+#[cfg(all(not(target_arch="wasm32"), not(feature="empty")))]
 pub use native::*;
 
 // wasm32版本，使用web实现
-#[cfg(all(target_feature="wasm32", not(feature="empty")))]
+#[cfg(all(target_arch="wasm32", not(feature="empty")))]
 mod web;
-#[cfg(all(target_feature="wasm32", not(feature="empty")))]
+#[cfg(all(target_arch="wasm32", not(feature="empty")))]
 pub use web::*;
 
 // 也可用feature指定为空实现
