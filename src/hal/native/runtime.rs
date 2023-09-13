56ebae53349afd9cb8831476eacc2b38a0a19d5c
wzjsun <530739162@qq.com>
2023-09-13 10:01:57 +0800

    fix: 单线程

--- src/hal/native/runtime.rs
+++ src/hal/native/runtime.rs
@@ -1,34 +1,58 @@
 use std::env;
 // use std::time::Instant;
 
-use pi_async_rt::rt::multi_thread::{MultiTaskRuntime, StealableTaskPool, MultiTaskRuntimeBuilder};
+// use pi_async_rt::{rt::multi_thread::{MultiTaskRuntime, StealableTaskPool, MultiTaskRuntimeBuilder}};
 // use pi_share::ShareMutex;
 
+#[cfg(not(feature = "single_thread"))]
 lazy_static! {
 	// pub static ref LOGS: ShareMutex<(Vec<String>, Instant)> = ShareMutex::new((Vec::new(), Instant::now()));
 
     // 多媒体运行时，多线程，不需要主动推
-    pub static ref MULTI_MEDIA_RUNTIME: MultiTaskRuntime<()> = {
+    pub static ref MULTI_MEDIA_RUNTIME: pi_async_rt::prelude::MultiTaskRuntime<()>  = {
         let count = match env::var("_ver") {
             Ok(r) => usize::from_str_radix(r.as_str(), 10).unwrap(),
             _ => num_cpus::get()
         };
-        let pool = StealableTaskPool::with(count, 0x8000, [1, 1], 3000);
+        let pool = pi_async_rt::prelude::StealableTaskPool::with(count, 0x8000, [1, 1], 3000);
         // 线程池：每个线程1M的栈空间，10ms 休眠，10毫秒的定时器间隔
-        let builder = MultiTaskRuntimeBuilder::new(pool).init_worker_size(count).set_worker_limit(count, count);
+        let builder = pi_async_rt::prelude::MultiTaskRuntimeBuilder::new(pool).init_worker_size(count).set_worker_limit(count, count);
         builder.build()
     };
 
 	// 渲染运行时，多线程，不需要主动推
-    pub static ref RENDER_RUNTIME: MultiTaskRuntime<()> = {
+    pub static ref RENDER_RUNTIME: pi_async_rt::prelude::MultiTaskRuntime<()>  = {
         let count = match env::var("_ver") {
             Ok(r) => usize::from_str_radix(r.as_str(), 10).unwrap(),
             _ => num_cpus::get()
         };
-        let pool = StealableTaskPool::with(count, 0x8000, [1, 1], 3000);
+        let pool = pi_async_rt::prelude::StealableTaskPool::with(count, 0x8000, [1, 1], 3000);
         // 线程池：每个线程1M的栈空间，10ms 休眠，10毫秒的定时器间隔
-        let builder = MultiTaskRuntimeBuilder::new(pool).init_worker_size(count).set_worker_limit(count, count);
+        let builder = pi_async_rt::prelude::MultiTaskRuntimeBuilder::new(pool).init_worker_size(count).set_worker_limit(count, count);
         builder.build()
     };
 }
+
+#[cfg(feature = "single_thread")]
+lazy_static! {
+	// pub static ref LOGS: ShareMutex<(Vec<String>, Instant)> = ShareMutex::new((Vec::new(), Instant::now()));
+
+    // 多媒体运行时，多线程，不需要主动推
+    pub static ref MULTI_MEDIA_RUNTIME: pi_async_rt::prelude::SingleTaskRuntime<()>  = {
+		let runner = pi_async_rt::prelude::SingleTaskRunner::default();
+		runner.into_local()
+    };
+
+	// 渲染运行时，多线程，不需要主动推
+    pub static ref RENDER_RUNTIME:  pi_async_rt::prelude::SingleTaskRuntime<()> = {
+        let runner = pi_async_rt::prelude::SingleTaskRunner::default();
+		runner.into_local()
+    };
+}
+
+
+//     let runner = SingleTaskRunner::default();
+//     let rt = runner.into_local();
+
+//     rt
 
