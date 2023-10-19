fee83fd0095afc275441edf9c12ce92dc28e5cc1
zhuy <zhuy>
2023-10-19 14:53:59 +0800

    fix: 重复资源加载

--- src/hal/native/mod.rs
+++ src/hal/native/mod.rs
@@ -6,17 +6,17 @@ use std::{
 use parking_lot::Mutex;
 use pi_async_rt::rt::AsyncValue;
 
+pub mod compressed_texture;
 pub mod file;
 pub mod font_brush;
 pub mod image;
 pub mod runtime;
-pub mod compressed_texture;
 
 // pub use basis_universal::TranscoderTextureFormat::*;
 
 lazy_static! {
     pub static ref LOAD_CB: RwLock<Option<Arc<dyn Fn(String) + Send + Sync>>> = RwLock::new(None);
-    pub static ref LOAD_MAP: Mutex<HashMap<String, AsyncValue<Vec<u8>>>> =
+    pub static ref LOAD_MAP: Mutex<HashMap<String, Vec<AsyncValue<Vec<u8>>>>> =
         Mutex::new(HashMap::new());
 }
 
@@ -25,18 +25,26 @@ pub fn init_load_cb(cb: Arc<dyn Fn(String) + Send + Sync>) {
 }
 
 pub fn on_load(path: &str, data: Vec<u8>) {
-    let v = LOAD_MAP.lock().remove(path).unwrap();
-    v.set(data);
+    let mut v = LOAD_MAP.lock().remove(path).unwrap();
+    v.drain(..).for_each(|v| {
+        v.set(data.clone());
+    });
 }
 
 pub fn create_async_value(path: &str) -> AsyncValue<Vec<u8>> {
-    let v = AsyncValue::new();
+    let mut lock = LOAD_MAP.lock();
+    if let Some(vec) = lock.get_mut(path) {
+        let v = AsyncValue::new();
+        vec.push(v.clone());
+        v
+    } else {
+        let v = AsyncValue::new();
+        lock.insert(path.to_string(), vec![v.clone()]);
 
-    LOAD_MAP.lock().insert(path.to_string(), v.clone());
+        if let Some(cb) = LOAD_CB.read().unwrap().as_ref() {
+            cb(path.to_string());
+        }
 
-    if let Some(cb) = LOAD_CB.read().unwrap().as_ref() {
-        cb(path.to_string());
+        v
     }
-
-    v
 }
