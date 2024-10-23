8f8d7f0b2b1678b1b3a303adba3cc579c129bd01
zhuy <zhuy>
2024-10-23 17:43:00 +0800

    feat: store native

--- src/hal/native/stroe.rs
+++ src/hal/native/stroe.rs
@@ -13,7 +13,7 @@ static STORE_GET_KEY: &'static str = "STORE_GET_KEY";
 static STORE_WRITE_KEY: &'static str = "STORE_WRITE_KEY";
 static STORE_DELETE_KEY: &'static str = "STORE_DELETE_KEY";
 lazy_static! {
-    pub static ref STROE_PATH: RwLock<Option<String>> = RwLock::new(None);
+    pub static ref STROE_PATH: RwLock<Option<PathBuf >> = RwLock::new(None);
 }
 
 pub async fn init_local_store() {
@@ -23,6 +23,9 @@ pub async fn init_local_store() {
 
     if let Ok(byte) = v.await {
         if let Ok(path) = String::from_utf8(byte.to_vec()) {
+            let path = PathBuf::from_str(&path).unwrap();
+            let path = path.join("sdf_font");
+            let _ = std::fs::create_dir_all(&path);
             *STROE_PATH.write() = Some(path);
         }
     }
@@ -33,10 +36,12 @@ pub async fn init_local_store() {
  */
 // tslint:disable-next-line:no-reserved-keywords
 pub async fn get(key: String) -> Option<Vec<u8>> {
+    println!("init_local_store get key: {}", key);
     if let Some(path) = STROE_PATH.read().as_ref() {
-        let path = PathBuf::from_str(path).unwrap();
-        let path = path.join("sdf_font").join(key);
+        let path = path.join(key);
+        println!("init_local_store get222 key: {:?}", path);
         if let Ok(data) = std::fs::read(path) {
+            println!("init_local_store get333 key: {:?}", data.len());
             return Some(data);
         }
     }
@@ -47,9 +52,10 @@ pub async fn get(key: String) -> Option<Vec<u8>> {
  * 往indexDb写数据
  */
 pub async fn write(key: String, data: Vec<u8>) {
+    println!("init_local_store write key: {}", key);
     if let Some(path) = STROE_PATH.read().as_ref() {
-        let path = PathBuf::from_str(path).unwrap();
-        let path = path.join("sdf_font").join(key);
+        let path = path.join(key);
+        println!("init_local_store write222 key: {:?}", path);
         let _ = std::fs::write(path, data);
     }
 }
@@ -58,9 +64,9 @@ pub async fn write(key: String, data: Vec<u8>) {
  * 从indexDb删除数据
  */
 pub async fn delete_key(key: String) {
+    println!("init_local_store delete_key: {}", key);
     if let Some(path) = STROE_PATH.read().as_ref() {
-        let path = PathBuf::from_str(path).unwrap();
-        let path = path.join("sdf_font").join(key);
+        let path = path.join(key);
         let _ = std::fs::remove_file(path);
     }
 }
